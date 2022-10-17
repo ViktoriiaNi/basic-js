b62ad851a9bfcb8d8cba77817af58ad6dc17de5d
ViktoriiaNi <viktorianigmatullina@gmail.com>
2022-10-18 01:47:09 +0400

    5

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr ) {
+  let sortedArr = arr.filter(num => num > 0).sort((a, b) => a - b)
+  let i = 0;
+  let result = arr.map(function(num) {
+    if (num == -1) return -1;
+    num = sortedArr[i];
+    i++
+    return num
+  })
+  return result
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(day) {  if (!day) {
+  return 'Unable to determine the time of year!';
+}
+if ((!(day instanceof Date)) || (Object.keys(day).length > 0)) {
+  throw new Error("Invalid date!");
+}
+
+let month = day.getMonth();
+if ((month == 0) || (month == 1) || (month == 11)) {
+return 'winter';
+}
+if ((month > 1) && (month < 5)) {
+return 'spring'
+}
+if ((month > 4) && (month < 8)) {
+return 'summer'
+}
+if ((month > 7) && (month < 11)) {
+return 'fall'}
 }
 
 module.exports = {
